975411bd698adee9262b48ac7bca89a2eb25939d
MaiconBolsoni <maicon.bolsoni@conexos.inf.br>
2025-02-28 22:20:51 -0300

    AJUSTE NO RENORTO DE DADOS

--- carga_bateria.py
+++ carga_bateria.py
@@ -1,23 +1,4 @@
 def carga_bateria(pcd, soc, pnet, nbrt, ncd, sd, soc_min, soc_max, cbat, pbatmax):
-    """
-    Função para calcular o carregamento da bateria.
-
-    Parâmetros:
-    pcd (float): Percentual de carga/descarga
-    soc (float): Estado de carga atual da bateria
-    pnet (float): Potência líquida da rede
-    nbrt (float): Eficiência de ida e volta
-    ncd (float): Eficiência de carga e descarga
-    sd (float): Taxa de auto-descarga
-    soc_min (float): Estado de carga mínimo
-    soc_max (float): Estado de carga máximo
-    cbat (float): Capacidade da bateria
-    pbatmax (float): Potência máxima da bateria
-
-    Retorna:
-    tuple: (pgrid, pbat, soc)
-    """
-
     x = 0.25  # Fração de hora utilizada
     soc_aux = soc
 
